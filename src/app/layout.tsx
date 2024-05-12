8b91c136cce295365434e87f92a1797e5ed1bd23
priv-hirai <hiraitakahiro0923@gmail.com>
2024-05-12 14:27:08 +0900

    feat: shadn ui導入

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
+import Header from '@/components/header';
 
-const inter = Inter({ subsets: ["latin"] });
+const notoSansJp = Noto_Sans_JP({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +16,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="ja">
+      <body className={notoSansJp.className}>
+        <Header />
         <main>
           {children}
         </main>
---
+++ src/components/header/index.tsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+    return (
+        <header>
+            <h1 className="text-4xl font-bold">Welcome to Next.js with Tailwind CSS</h1>
+            <p className="text-xl">Get started by editing <code>pages/index.tsx</code></p>
+        </header>
+    );
+}
+
+export default Header;
