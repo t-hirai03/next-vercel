bf19678541776c679d9b0dd764af59262b2ae5ae
平井 <hiraitakahiro0923@gmail.com>
2024-05-12 23:06:08 +0900

    feat: v0導入

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,8 +2,7 @@
 const Header = () => {
     return (
         <header>
-            <h1 className="text-4xl font-bold">Welcome to Next.js with Tailwind CSS</h1>
-            <p className="text-xl">Get started by editing <code>pages/index.tsx</code></p>
+            <p>Header</p>
         </header>
     );
 }
