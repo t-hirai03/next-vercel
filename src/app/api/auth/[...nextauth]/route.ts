55138ef5dc327078d902bdee96f0860d2b5cc91d
priv-hirai <hiraitakahiro0923@gmail.com>
2024-05-19 23:13:37 +0900

    fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,14 +5,14 @@ import GithubProvider from 'next-auth/providers/github';
 const handler = NextAuth({
 	secret: process.env.NEXTAUTH_SECRET,
 	providers: [
-		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
-		}),
-		GithubProvider({
-			clientId: process.env.GITHUB_ID || '',
-			clientSecret: process.env.GITHUB_SECRET || '',
-		}),
+		// GoogleProvider({
+		// 	clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+		// 	clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+		// }),
+		// GithubProvider({
+		// 	clientId: process.env.GITHUB_ID || '',
+		// 	clientSecret: process.env.GITHUB_SECRET || '',
+		// }),
 	]
 });
 
