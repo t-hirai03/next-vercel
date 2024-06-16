fbf817df7340c1cdb44d60f258d4feec06464d5a
priv-hirai <hiraitakahiro0923@gmail.com>
2024-06-16 18:12:17 +0900

    fix

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/router";
 import styles from "./index.module.scss";
 import { useSession, signOut } from "next-auth/react";
 import Image from "next/image";
