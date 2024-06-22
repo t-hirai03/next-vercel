e1190335701887138e0da28fb56674983fedae78
priv-hirai <hiraitakahiro0923@gmail.com>
2024-06-22 16:59:36 +0900

    fix: スプラトゥーンカードUI変更

--- src/components/top/splatoon/index.tsx
+++ src/components/top/splatoon/index.tsx
@@ -7,6 +7,7 @@ import { ja } from "date-fns/locale";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const Splatoon = () => {
   const salmonRunData: salmonRunData[] | null = useFetchSalmonRun();
@@ -22,43 +23,51 @@ const Splatoon = () => {
                 <CardHeader>
                   <CardTitle className={styles.title}>{data.stage.name}</CardTitle>
                   <CardDescription>
-                    スケジュール:{" "}
                     {format(new Date(data.start_time), "yyyy/MM/dd HH:mm", { locale: ja })} -{" "}
                     {format(new Date(data.end_time), "yyyy/MM/dd HH:mm", { locale: ja })}
                   </CardDescription>
                 </CardHeader>
                 <CardContent>
-                  <Image
-                    src={data.stage.image}
-                    alt={data.stage.name}
-                    width={800}
-                    height={450}
-                    className={styles.thumbnail}
-                    priority={true}
-                  />
-                  <div>
-                    <CardDescription>Big Run: {data.is_big_run ? "あり" : "なし"}</CardDescription>
-                    <CardDescription>Boss: {data.boss.name}</CardDescription>
-                    <CardDescription>武器一覧:</CardDescription>
-                    <ul className={styles.weapons}>
-                      {data.weapons.map((weapon, wIndex) => (
-                        <li key={wIndex} className={styles.weapons_item}>
-                          <TooltipProvider>
-                            <Tooltip>
-                              <TooltipTrigger>
-                                <Avatar className={styles.avatar}>
-                                  <AvatarImage src={weapon.image} alt={weapon.name} />
-                                  <AvatarFallback>?</AvatarFallback>
-                                </Avatar>
-                              </TooltipTrigger>
-                              <TooltipContent className={styles.tooltip}>
-                                {weapon.name}
-                              </TooltipContent>
-                            </Tooltip>
-                          </TooltipProvider>
-                        </li>
-                      ))}
-                    </ul>
+                  <div className={styles.thumbnail}>
+                    <Skeleton className={styles.skeleton} />
+                    <Image
+                      src={data.stage.image}
+                      alt={data.stage.name}
+                      width={800}
+                      height={450}
+                      className={styles.image}
+                      priority={true}
+                    />
+                  </div>
+                  <div className={styles.info}>
+                    <div>
+                      <p className={styles.description}><span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />ビッグラン</p> {data.is_big_run ? "あり" : "なし"}
+                    </div>
+                    <div>
+                      <p className={styles.description}><span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />ボス</p> {data.boss.name}
+                    </div>
+                    <div>
+                      <p className={styles.description}><span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />武器一覧</p>
+                      <ul className={styles.weapons}>
+                        {data.weapons.map((weapon, wIndex) => (
+                          <li key={wIndex} className={styles.weapons_item}>
+                            <TooltipProvider>
+                              <Tooltip>
+                                <TooltipTrigger>
+                                  <Avatar className={styles.avatar}>
+                                    <AvatarImage src={weapon.image} alt={weapon.name} />
+                                    <AvatarFallback>?</AvatarFallback>
+                                  </Avatar>
+                                </TooltipTrigger>
+                                <TooltipContent className={styles.tooltip}>
+                                  {weapon.name}
+                                </TooltipContent>
+                              </Tooltip>
+                            </TooltipProvider>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
                   </div>
                 </CardContent>
               </Card>
