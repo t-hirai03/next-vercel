e287916c75811d55772b3f07fe41c48c902c20bd
priv-hirai <hiraitakahiro0923@gmail.com>
2024-06-23 01:17:36 +0900

    fix:マイページ調整

--- src/components/customers/mypage/index.tsx
+++ src/components/customers/mypage/index.tsx
@@ -1,22 +1,29 @@
 import Image from 'next/image'
 import { useSession } from 'next-auth/react'
 
+import styles from './index.module.scss'
+
 const MyPage = () => {
   const { data: session } = useSession()
 
   return (
     <div className="l-wrapper">
-      <h2>マイページ</h2>
-      <p>ようこそ、{session?.user?.name}さん</p>
-      <p>メールアドレス：{session?.user?.email}</p>
-      <div>
+      <h2 className={styles.title}>ユーザー情報</h2>
+      <div className={styles.profile}>
         <Image
           src={session?.user?.image ?? ''}
           alt=""
           width={100}
           height={100}
           style={{ borderRadius: '50%' }}
+          className={styles.icon}
         />
+        <div className={styles.info}>
+          <p>
+            <span className={styles.name}>{session?.user?.name}</span>さん
+          </p>
+          <p>{session?.user?.email}</p>
+        </div>
       </div>
     </div>
   )
