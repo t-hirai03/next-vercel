f8d294b49781784a1e8753a737e45577f9a6f404
priv-hirai <hiraitakahiro0923@gmail.com>
2024-06-23 13:32:57 +0900

    feat: gaにユーザー情報計測する処理追加

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useSession, signOut } from 'next-auth/react'
+import { useEffect } from 'react'
 
 import {
   DropdownMenu,
@@ -19,6 +20,24 @@ import styles from './index.module.scss'
 const Header = () => {
   const { data: session, status } = useSession()
   const router = useRouter()
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && window.dataLayer && session?.user) {
+      // dataLayerにユーザー情報を送信
+      window.dataLayer.push({
+        event: 'userInformation',
+        user_name: session?.user?.name,
+        user_email: session?.user?.email,
+        user_image: session?.user?.image,
+      })
+
+      // cookieにユーザー情報を保存
+      document.cookie = `user_name=${session?.user?.name}`
+    } else {
+      // cookieに保存されたユーザー情報を削除
+      document.cookie = `user_name=; expires=Thu, 01 Jan 1970 00:00:00 UTC`
+    }
+  }, [session])
 
   return (
     <header className={styles.header}>
