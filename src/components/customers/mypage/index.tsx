1a06054cd4ce12293bab4987ade2f877ac6bf495
priv-hirai <hiraitakahiro0923@gmail.com>
2024-07-06 20:54:44 +0900

    fix:マイページレスポンシブ対応

--- src/components/customers/mypage/index.tsx
+++ src/components/customers/mypage/index.tsx
@@ -5,26 +5,30 @@ import styles from './index.module.scss'
 
 const MyPage = () => {
   const { data: session } = useSession()
+  console.log(session)
 
   return (
     <div className="l-wrapper">
-      <h2 className={styles.title}>ユーザー情報</h2>
-      <div className={styles.profile}>
-        <Image
-          src={session?.user?.image ?? ''}
-          alt=""
-          width={100}
-          height={100}
-          style={{ borderRadius: '50%' }}
-          className={styles.icon}
-        />
-        <div className={styles.info}>
-          <p>
-            <span className={styles.name}>{session?.user?.name}</span>さん
-          </p>
-          <p>{session?.user?.email}</p>
+      <section>
+        <h2 className={styles.title}>ユーザー情報</h2>
+        <div className={styles.profile}>
+          <Image
+            src={session?.user?.image ?? ''}
+            alt=""
+            width={100}
+            height={100}
+            style={{ borderRadius: '50%' }}
+            className={styles.icon}
+          />
+          <div className={styles.info}>
+            <p className={styles.text}>
+              <span className={styles.name}>{session?.user?.name}</span>さん
+            </p>
+            <p className={styles.email}>{session?.user?.email}</p>
+          </div>
         </div>
-      </div>
+      </section>
+      <section>{/* {session.user.username} */}</section>
     </div>
   )
 }
