28c9535968424a34dee5c0352b338a62154a0257
priv-hirai <hiraitakahiro0923@gmail.com>
2024-07-06 21:59:34 +0900

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
+'use client'
+
 import './scss/globals.scss'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import { GoogleAnalytics } from '@next/third-parties/google'
-import { ThemeProvider } from 'next-themes'
+import { ThemeProvider as NextThemesProvider } from 'next-themes' // eslint-disable-line import/no-extraneous-dependencies
 
 import Header from '@/components/header'
 import Meta from '@/components/meta'
@@ -19,10 +21,14 @@ export default function RootLayout({
       <Meta />
       <NextAuthProvider>
         <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID || ''} />
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <NextThemesProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+        >
           <Header />
           <main>{children}</main>
-        </ThemeProvider>
+        </NextThemesProvider>
       </NextAuthProvider>
     </>
   )
